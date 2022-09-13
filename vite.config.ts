51582028bf7be90868c64a9a959bc40fe5f43622
fidelda <fidelda@uni-bremen.de>
2022-09-13 10:05:24 +0200

    removed base from config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fidelda.github.io/', // base URL to the webiste
   plugins: [vue()],
   resolve: {
     alias: {
